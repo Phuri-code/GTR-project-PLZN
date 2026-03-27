4a3310f6c33c67e8c7f0f79e0cd8be5c72d5f8d5
Phuri-code <phuritry2222@gmail.com>
2026-03-27 14:42:17 +0700

    please

--- Algorithm.java
+++ Algorithm.java
@@ -1,3 +1,29 @@
 public class Algorithm {
     // sdfghiop khasdkhfkasdf
+    int[] sample = {};
+    int[] input;
+    int[] output;
+
+    //Task2
+    Algorithm(int[] input) {
+    }
+    
+    // Task3 
+    int[] sum(int[] input){
+        return new int[0];
+    }
+
+    //Task4
+    int[] max(int[] input){
+        return 0;
+    }
+
+    // Task5 
+    int main(int[] input){
+        return 0;
+    
+
+
+
+
 }
