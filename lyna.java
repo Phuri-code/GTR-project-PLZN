2d3f0503b7d26067f60c34819c1f1af391c40c01
voeunlyna-cyber <voeunlyna@gmail.com>
2026-04-03 14:36:12 +0700

    hi

---
+++ lyna.java
@@ -0,0 +1,9 @@
+public class lyna {
+    public static void main(String[] args) {
+        // Task 1: Implement a sorting algorithm (e.g., bubble sort, quicksort, merge sort).
+        int[] sample = {5, 2, 9, 1, 5, 6};
+        int[] sortedSample = bubbleSort(sample);
+        System.out.println("Sorted Sample: " + Arrays.toString(sortedSample));
+    }
+    
+}
